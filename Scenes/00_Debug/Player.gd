3cbfdf03f37a25d4fd78c74aa16da12b75473be4
evan-poster <evan.poster@gmail.com>
2024-04-24 16:14:29 -0400

    Reworked player code to properly handle state (Floor/Wall/Air)

--- Scenes/00_Debug/Player.gd
+++ Scenes/00_Debug/Player.gd
@@ -1,27 +1,30 @@
 extends CharacterBody2D
 
 """
-	Update player state based on user input and physics
+	Update player current_state based on user input and physics
 	
 	1. Determine player intent via input
-	2. Determine player object state
-	3. Apply state based on intent within context of physics
+	2. Determine player object current_state
+	3. Apply current_state based on intent within context of physics
 
 """
 
-enum PlayerState {
-	IDLE,
-	RUN,
-	JUMPING,
-	FALLING,
+# State priority: Floor > Wall > AIR
+
+enum State {
+	FLOOR,
+	AIR,
+	WALL,
 }
 
+# State/Flag Variables
 var last_state
-var state = PlayerState.IDLE
-var weapon_drawn = false
+var current_state = State.AIR
+# var weapon_drawn = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Physics Parameters
+const RUN_SPEED = 425.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -29,54 +32,84 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 
-	last_state = state
-	state = PlayerState.IDLE
-
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if velocity.y > 0:
-			state = PlayerState.FALLING
-		else:
-			state = PlayerState.JUMPING
-
-	# Handle jump.
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		state = PlayerState.JUMPING
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("player_left", "player_right")
-	if direction:
-		velocity.x = direction * SPEED
-		# Set player sprite direction based on direction pos/neg value
-		if direction > 0:
-			$AnimatedSprite2D.flip_h = false
-		elif direction < 0:
-			$AnimatedSprite2D.flip_h = true
-		if is_on_floor():
-			if direction == 0:
-				state = PlayerState.IDLE
-			else:
-				state = PlayerState.RUN
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Track last state held by player
+	last_state = current_state
+	
+	# Handle current_state
+	match current_state:
+		State.FLOOR:
+			print("Current State: FLOOR")
+			process_floor(delta)
+		State.AIR:
+			print("Current State: AIR")
+			process_air(delta)
+		State.WALL:
+			print("Current State: WALL")
+			process_wall(delta)
 
 	# Apply physics
 	move_and_slide()
 
-	# Process based on state
-	match state:
-		PlayerState.IDLE:
-			if state != last_state:
-				$AnimatedSprite2D.play("idle")
-		PlayerState.RUN:
-			if state != last_state:
+#
+# State Processing Functions
+#
+
+func process_floor(delta):
+	# Confirm we're still on the ground
+	if is_on_floor():
+		# Jump interrupts running/idle
+		if Input.is_action_just_pressed("player_jump"):
+			velocity.y = JUMP_VELOCITY * 0.80
+			current_state = State.AIR
+		
+		# Move left/right regardless
+		move_left_right()
+
+		# Update sprite
+		if self.velocity.x != 0:
+			if $AnimatedSprite2D.animation != "run":
 				$AnimatedSprite2D.play("run")
-		PlayerState.JUMPING:
-			if state != last_state:
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		else:
+			$AnimatedSprite2D.play("idle")
+
+	else:
+		current_state = State.AIR
+
+
+func process_wall(_delta):
+	# Check if we are against a wall
+	if is_on_wall():
+		pass
+
+
+func process_air(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		move_left_right()
+		
+		# Ensure we are using 'jump' or 'fall' based on velocity.y
+		if velocity.y < 0:
+			if $AnimatedSprite2D.animation != "jump":
 				$AnimatedSprite2D.play("jump")
-		PlayerState.FALLING:
-			if state != last_state:
+		else:
+			if $AnimatedSprite2D.animation != "fall":
 				$AnimatedSprite2D.play("fall")
+		# Ensure our sprite is facing the correct way
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+	else:
+		current_state = State.FLOOR
+
+
+#
+# Helper functions
+#
+
+func move_left_right():
+	var direction = Input.get_axis("player_left", "player_right")
+	if direction:
+		velocity.x = direction * RUN_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, RUN_SPEED)
+
